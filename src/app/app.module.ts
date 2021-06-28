0a56911449d6b62f0feb60e09256ad81fc40fa4e
DESKTOP-8PIBK0G\sw <rj9101@me-zoo.com>
2021-06-28 17:11:07 +0900

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PDataComponent } from './p-data/p-data.component';
+import { DListComponent } from './d-list/d-list.component';
+import { PeriodComponent } from './period/period.component';
+import { ControllerComponent } from './controller/controller.component';
+import { EcgComponent } from './ecg/ecg.component';
+import { RespirationComponent } from './respiration/respiration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PDataComponent,
+    DListComponent,
+    PeriodComponent,
+    ControllerComponent,
+    EcgComponent,
+    RespirationComponent
   ],
   imports: [
     BrowserModule,
